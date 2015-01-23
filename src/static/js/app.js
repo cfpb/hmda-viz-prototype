44c5948cd4ccc58368a9768483fe695c1a6a6549
Andrew <awolfe76@gmail.com>
2015-01-23 08:37:19 -0500

    remove feature detection

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,13 +1,3 @@
-// Count all features included in the test page.
-$('.feature-list').append(
-  '<section class="feature-list_item block block__padded-top block__border-top">' +
-  '<h1>jQuery</h1>' +
-  '<p>jQuery is working and counts a total of ' +
-  '<strong>' + $('.feature-list_item').size() + '</strong> ' +
-  'cf-components.</p>' +
-  '</section>'
-);
-
 // get data for each select input
 // call the corresponding mustache template and fill the input
 // set the first option as selected
