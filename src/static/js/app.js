5835ae29ce525e1d9b87ca0f5844e17ad450e657
Andrew <awolfe76@gmail.com>
2015-03-02 09:54:39 -0500

    added sass

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,15 +1,15 @@
 /**
-	* get data for each select input
-	* call the corresponding mustache template and fill the input
-	* set the first option as selected
-	*/
+    * get data for each select input
+    * call the corresponding mustache template and fill the input
+    * set the first option as selected
+    */
 function getUIData() {
-	'use strict';
+    'use strict';
   $('select').each(function() {
     var selectID = this.id;
     // get <select> data
     $.get(selectID + '.json', function(data) {
-    	// get template
+        // get template
       $.get('/hmda-viz-prototype/templates/selects.html', function(templates) {
         var template = $(templates).filter('#' + selectID).html();
         var html = Mustache.to_html(template, data);
@@ -22,17 +22,17 @@ function getUIData() {
 }
 
 /**
-	* get data for the table
-	* call the corresponding mustache template and fill the input
-	* set the first option as selected
-	*/
+    * get data for the table
+    * call the corresponding mustache template and fill the input
+    * set the first option as selected
+    */
 function getTableData(table) {
-	'use strict';
-	// get <table> data
+    'use strict';
+    // get <table> data
   $.get(table + '.json', function(data) {
-  	// get template
+    // get template
     $.get('/hmda-viz-prototype/templates/' + table + '.html', function(templates) {
-    	//var template = $(templates).filter('#' + table + '-new').html();
+        //var template = $(templates).filter('#' + table + '-new').html();
       var template = $(templates).filter('#' + table).html();
       var html = Mustache.to_html(template, data);
       $('#' + table).html(html);
@@ -41,10 +41,10 @@ function getTableData(table) {
 }
 
 /** 
-	* update the button link on the form pages
-	*/
+    * update the button link on the form pages
+    */
 function setLink() {
-	'use strict';
+    'use strict';
   var newURL = '';  // needed on first page, year and state make the url
   $('select').each(function() {
     newURL += $(this).val().replace(' ', '-').toLowerCase() + '/';
@@ -53,7 +53,7 @@ function setLink() {
 }
 
 $( document ).ready(function() {
-	'use strict';
+    'use strict';
   jQuery.ajaxSetup({async:false});  // no async!
 
   var urlPath = window.location.pathname.split('/');
