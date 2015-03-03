c207305bc3bab3a6e61d37a981092432ff7339b6
David Roell <breeroell@gmail.com>
2015-03-03 11:09:56 -0500

    generalizing controller

--- processing/constructor.py
+++ processing/constructor.py
@@ -16,62 +16,168 @@ class constructor(object):
 	pass
 
 class report_4x(constructor):
-	def __init__(self):
+	def __init__(self, report, selector):
+		self.year = selector.report_list['year'][1]
 		self.parsed = parse() #for parsing inputs from rows
 		self.connection = connect() #connects to the DB
 		self.queries = queries() #query text for all tables
 		self.agg = agg() #aggregation functions for all tables
+		self.json_builder = self.JSON_constructor_return(report)
+		self.parse_function = self.parse_return(report)
+		self.query_string = self.query_return(self.year, report)
+		self.aggregation = self.aggregation_return(self.year, report)
+		self.count_string = self.count_return(self.year, report)
+		self.report_number = report
 
 
-	def report_x(self, selector, report_number, query_string, count_string, parse_function, json_builder):
-		#count_string_2012 = 'count_rows_47_2012'
-		#count_string_2013 = 'count_rows_47_2013'
-		#query_string_2012 = 'table_4_7_2012'
-		#query_string_2013 = 'table_4_7_2013'
-		#parse_function = 'parse_t4x'
-		#json_builder = 'table_4x_builder'
+	def aggregation_return(self, year, report_number):
+		if report_number == 'A 3-1':
+			return 'build_report_31'
+		elif report_number == 'A 3-2':
+			return 'build_report_32'
+		elif report_number[:3] == 'A 4':
+			return 'build_report4x'
 
-		table_number = report_number[2:]
-		if report_number[0] == 'A':
+	def JSON_constructor_return(self, report_number):
+		if report_number == 'A 3-1':
+			return 'table_31_builder'
+		elif report_number == 'A 3-2':
+			return 'table_32_builder'
+		elif report_number[:3] == 'A 4':
+			return 'table_4x_builder'
+
+	def parse_return(self, report_number):
+		if report_number == 'A 3-1':
+			return 'parse_t31'
+		elif report_number == 'A 3-2':
+			return 'parse_t32'
+		elif report_number[:3] == 'A 4':
+			return 'parse_t4x'
+
+	def query_return(self, year, report_number):
+		if year == '2013':
+			if report_number == 'A 3-1':
+				return 'table_3_1_2013'
+			elif report_number == 'A 3-2':
+				return 'table_3_2_2013'
+			elif report_number == 'A 4-1':
+				return 'table_4_1_2013'
+			elif report_number == 'A 4-2':
+				return 'table_4_2_2013'
+			elif report_number == 'A 4-3':
+				return 'table_4_3_2013'
+			elif report_number == 'A 4-4':
+				return 'table_4_4_2013'
+			elif report_number == 'A 4-5':
+				return 'table_4_5_2013'
+			elif report_number == 'A 4-6':
+				return 'table_4_6_2013'
+			elif report_number == 'A 4-7':
+				return 'table_4_7_2013'
+		elif year == '2012':
+			if report_number == 'A 3-1':
+				return 'table_3_1_2012'
+			elif report_number == 'A 3-2':
+				return 'table_3_2_2012'
+			elif report_number == 'A 4-1':
+				return 'table_4_1_2012'
+			elif report_number == 'A 4-2':
+				return 'table_4_2_2012'
+			elif report_number == 'A 4-3':
+				return 'table_4_3_2012'
+			elif report_number == 'A 4-4':
+				return 'table_4_4_2012'
+			elif report_number == 'A 4-5':
+				return 'table_4_5_2012'
+			elif report_number == 'A 4-6':
+				return 'table_4_6_2012'
+			elif report_number == 'A 4-7':
+				return 'table_4_7_2012'
+
+	def count_return(self, year, report_number):
+		if year == '2013':
+			if report_number == 'A 3-1':
+				return 'count_rows_2013'
+			elif report_number == 'A 3-2':
+				return 'count_rows_2013'
+			elif report_number == 'A 4-1':
+				return 'count_rows_41_2013'
+			elif report_number == 'A 4-2':
+				return 'count_rows_42_2013'
+			elif report_number == 'A 4-3':
+				return 'count_rows_43_2013'
+			elif report_number == 'A 4-4':
+				return 'count_rows_44_2013'
+			elif report_number == 'A 4-5':
+				return 'count_rows_45_2013'
+			elif report_number == 'A 4-6':
+				return 'count_rows_46_2013'
+			elif report_number == 'A 4-7':
+				return 'count_rows_47_2013'
+		elif year == '2012':
+			if report_number == 'A 3-1':
+				return 'count_rows_2012'
+			elif report_number == 'A 3-2':
+				return 'count_rows_2012'
+			elif report_number == 'A 4-1':
+				return 'count_rows_41_2012'
+			elif report_number == 'A 4-2':
+				return 'count_rows_42_2012'
+			elif report_number == 'A 4-3':
+				return 'count_rows_43_2012'
+			elif report_number == 'A 4-4':
+				return 'count_rows_44_2012'
+			elif report_number == 'A 4-5':
+				return 'count_rows_45_2012'
+			elif report_number == 'A 4-6':
+				return 'count_rows_46_2012'
+			elif report_number == 'A 4-7':
+				return 'count_rows_47_2012'
+
+	def report_x(self, MSA):
+		table_number = self.report_number[2:]
+		if self.report_number[0] == 'A':
 			report_type = 'Aggregate'
-		elif report_number[0] == 'D':
+		elif self.report_number[0] == 'D':
 			report_type = 'Disclosure'
-		elif report_number[0] == 'N':
+		elif self.report_number[0] == 'N':
 			report_type = 'National'
 
-		#function_return(self, selector.report_list['year'][1], report_number)
 		cur = self.connection.connect() #creates cursor object connected to HMDAPub2012 sql database, locally hosted postgres
 
-		for MSA in selector.report_list[report_number]:
-			build_X = build()
-			build_X.set_msa_names(cur) #builds a list of msa names as a dictionary
-			location = (MSA,) #pass the MSA nubmers as a tuple to Psycopg2 (doesn't take singletons)
+		#for MSA in selector.report_list[report_number]: #take this loop out
+		build_X = build()
+		build_X.set_msa_names(cur) #builds a list of msa names as a dictionary
+		location = (MSA,) #pass the MSA nubmers as a tuple to Psycopg2 (doesn't take singletons)
 
-			SQL = getattr(self.queries, count_string)()
+		SQL = getattr(self.queries, self.count_string)()
+		cur.execute(SQL, location)
+		count = int(cur.fetchone()[0])
+
+		if count > 0:
+			print count, 'LAR rows in MSA %s, for report %s, in %s' %(MSA, self.report_number, self.year)
+
+			SQL = getattr(self.queries, self.query_string)()
 			cur.execute(SQL, location)
-			count = int(cur.fetchone()[0])
+			for num in range(0, count):
+				row = cur.fetchone()
+				getattr(self.parsed, self.parse_function)(row)
+				if num == 0:
+					build_X.set_header(self.parsed.inputs, MSA, report_type, table_number)
+					table_X = getattr(build_X, self.json_builder)()
 
-			if count > 0:
-				print count, 'LAR rows in MSA %s, for report %s, in %s' %(MSA, report_number, selector.report_list['year'][1])
-
-				SQL = getattr(self.queries, query_string)()
-				cur.execute(SQL, location)
-				for num in range(0, count):
-					row = cur.fetchone()
-					getattr(self.parsed, parse_function)(row)
-					if num == 0:
-						build_X.set_header(self.parsed.inputs, MSA, report_type, table_number)
-						table_X = getattr(build_X, json_builder)()
-					self.agg.build_report4x(table_X, self.parsed.inputs)
-				path = "json" + "/" +table_X['type']+"/"+table_X['year']+"/"+build_X.get_state_name(table_X['msa']['state']).replace(' ', '-').lower()+"/"+build_X.msa_names[MSA].replace(' ', '-').lower()+"/"+table_X['table']
-				if not os.path.exists(path): #check if path exists
-					os.makedirs(path) #if path not present, create it
-				build_X.write_JSON(table_X['table']+'.json', table_X, path)
-				#use an if exists check for jekyll files
-				build_X.jekyll_for_report(path) #create and write jekyll file to report path
-				#year in the path is determined by the asofdate in the LAR entry
-				path2 = "json"+"/"+table_X['type']+"/"+table_X['year']+"/"+build_X.get_state_name(table_X['msa']['state']).replace(' ', '-').lower()+"/"+build_X.msa_names[MSA].replace(' ', '-').lower() #set path for writing the jekyll file to the msa directory
-				build_X.jekyll_for_msa(path2) #create and write jekyll file to the msa path
+				#this line needs generalization
+				getattr(self.agg, self.aggregation)(table_X, self.parsed.inputs)
+				#self.agg.build_report4x(table_X, self.parsed.inputs)
+			path = "json" + "/" +table_X['type']+"/"+table_X['year']+"/"+build_X.get_state_name(table_X['msa']['state']).replace(' ', '-').lower()+"/"+build_X.msa_names[MSA].replace(' ', '-').lower()+"/"+table_X['table']
+			if not os.path.exists(path): #check if path exists
+				os.makedirs(path) #if path not present, create it
+			build_X.write_JSON(table_X['table']+'.json', table_X, path)
+			#use an if exists check for jekyll files
+			build_X.jekyll_for_report(path) #create and write jekyll file to report path
+			#year in the path is determined by the asofdate in the LAR entry
+			path2 = "json"+"/"+table_X['type']+"/"+table_X['year']+"/"+build_X.get_state_name(table_X['msa']['state']).replace(' ', '-').lower()+"/"+build_X.msa_names[MSA].replace(' ', '-').lower() #set path for writing the jekyll file to the msa directory
+			build_X.jekyll_for_msa(path2) #create and write jekyll file to the msa path
 '''
 	def report_47(self, selector):
 		cur = self.connection.connect() #creates cursor object connected to HMDAPub2012 sql database, locally hosted postgres
--- processing/controller.py
+++ processing/controller.py
@@ -22,121 +22,18 @@ cur = connection.connect() #creates cursor object connected to HMDAPub2012 sql d
 selector.get_report_lists('MSAinputs2013.csv') #fills the dictionary of lists of reports to be generated
 build_msa = build() #instantiate the build object
 #need to make a switch to build aggregate and disclosure MSA in state lists
-build_msa.msas_in_state(cur, selector, 'aggregate') #creates a list of all MSAs in each state and places the file in the state's aggregate folder
-build_msa.msas_in_state(cur, selector, 'disclosure')#creates a list of all MSAs in each state and places the file in the state's disclosure folder
+#build_msa.msas_in_state(cur, selector, 'aggregate') #creates a list of all MSAs in each state and places the file in the state's aggregate folder
+#build_msa.msas_in_state(cur, selector, 'disclosure')#creates a list of all MSAs in each state and places the file in the state's disclosure folder
 
-def aggregation_return(year, report_number):
-	if report_number == 'A 3-1':
-		return 'build_report_31'
-	elif report_number == 'A 3-2':
-		return 'build_report_32'
-	elif report_number[:3] == 'A 4':
-		return 'build_report4x'
-
-def JSON_constructor_return(report_number):
-	if report_number == 'A 3-1':
-		return 'table_31_builder'
-	elif report_number == 'A 3-2':
-		return 'table_32_builder'
-	elif report_number[:3] == 'A 4':
-		return 'table_4x_builder'
-def parse_return(report_number):
-	if report_number == 'A 3-1':
-		return 'parse_t31'
-	elif report_number == 'A 3-2':
-		return 'parse_t32'
-	elif report_number[:3] == 'A 4':
-		return 'parse_t4x'
-
-def query_return(year, report_number):
-	if year == '2013':
-		if report_number == 'A 3-1':
-			return 'table_3_1_2013'
-		elif report_number == 'A 3-2':
-			return 'table_3_2_2013'
-		elif report_number == 'A 4-1':
-			return 'table_4_1_2013'
-		elif report_number == 'A 4-2':
-			return 'table_4_2_2013'
-		elif report_number == 'A 4-3':
-			return 'table_4_3_2013'
-		elif report_number == 'A 4-4':
-			return 'table_4_4_2013'
-		elif report_number == 'A 4-5':
-			return 'table_4_5_2013'
-		elif report_number == 'A 4-6':
-			return 'table_4_6_2013'
-		elif report_number == 'A 4-7':
-			return 'table_4_7_2013'
-	elif year == '2012':
-		if report_number == 'A 3-1':
-			return 'table_3_1_2012'
-		elif report_number == 'A 3-2':
-			return 'table_3_2_2012'
-		elif report_number == 'A 4-1':
-			return 'table_4_1_2012'
-		elif report_number == 'A 4-2':
-			return 'table_4_2_2012'
-		elif report_number == 'A 4-3':
-			return 'table_4_3_2012'
-		elif report_number == 'A 4-4':
-			return 'table_4_4_2012'
-		elif report_number == 'A 4-5':
-			return 'table_4_5_2012'
-		elif report_number == 'A 4-6':
-			return 'table_4_6_2012'
-		elif report_number == 'A 4-7':
-			return 'table_4_7_2012'
-
-def count_return(year, report_number):
-	if year == '2013':
-		if report_number == 'A 3-1':
-			return 'count_rows_2013'
-		elif report_number == 'A 3-2':
-			return 'count_rows_2013'
-		elif report_number == 'A 4-1':
-			return 'count_rows_41_2013'
-		elif report_number == 'A 4-2':
-			return 'count_rows_42_2013'
-		elif report_number == 'A 4-3':
-			return 'count_rows_43_2013'
-		elif report_number == 'A 4-4':
-			return 'count_rows_44_2013'
-		elif report_number == 'A 4-5':
-			return 'count_rows_45_2013'
-		elif report_number == 'A 4-6':
-			return 'count_rows_46_2013'
-		elif report_number == 'A 4-7':
-			return 'count_rows_47_2013'
-	elif year == '2012':
-		if report_number == 'A 3-1':
-			return 'count_rows_2012'
-		elif report_number == 'A 3-2':
-			return 'count_rows_2012'
-		elif report_number == 'A 4-1':
-			return 'count_rows_41_2012'
-		elif report_number == 'A 4-2':
-			return 'count_rows_42_2012'
-		elif report_number == 'A 4-3':
-			return 'count_rows_43_2012'
-		elif report_number == 'A 4-4':
-			return 'count_rows_44_2012'
-		elif report_number == 'A 4-5':
-			return 'count_rows_45_2012'
-		elif report_number == 'A 4-6':
-			return 'count_rows_46_2012'
-		elif report_number == 'A 4-7':
-			return 'count_rows_47_2012'
-
-
-report_4x = report_4x()
 #report_3x = report_3x()
 selector.report_list['A 4-2'] = ['29620']
 selector.report_list['A 4-1'] = ['29620']
 
 report_list = ['A 4-2', 'A 4-1']
-for report in report_list:
-	report_4x.report_x(selector, report, query_return(selector.report_list['year'][1], report), count_return(selector.report_list['year'][1], report), parse_return(report), JSON_constructor_return(report)) #variabalize funciton inputs!!!!
+for report in report_list: #loop over a list of report names
+	for MSA in selector.report_list[report]: #loop through MSAs flagged for report generation
+		report_x = report_4x(report, selector) #instantiate class and set function strings
+		report_x.report_x(MSA) #variabalize funciton inputs!!!!
 
 
 #report_4x.report_47(selector)
