04024116773be827b41d769d1e4b0c13aa512d4b
Andrew <awolfe76@gmail.com>
2015-04-06 11:55:45 -0400

    fixing ie download of the csv

--- js/app.js
+++ js/app.js
@@ -240,7 +240,42 @@ $( document ).ready(function() {
         }
     });
 
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+           // IE 12 => return version number
+           return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+}
+
     $('#csv').click(function() {
-        window.open('data:text/csv;charset=utf-8,' + escape(theCSV));
+        if (detectIE !== false) {
+            console.log('no ie');
+            window.open('data:text/csv;charset=utf-8,' + escape(theCSV));
+        } else {
+            var oWin = window.open();
+            oWin.document.write('sep=,\r\n' + theCSV);
+            oWin.document.close();
+            oWin.document.execCommand('SaveAs', true, fileName + ".csv");
+            oWin.close();
+        }
     });
 });
