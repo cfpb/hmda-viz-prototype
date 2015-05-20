fbf2beed7e6b27f429afc109a956d2218b075553
David Roell <breeroell@gmail.com>
2015-05-20 12:48:29 -0400

    modified gitignore to include Alabama disclosure

--- processing/controller.py
+++ processing/controller.py
@@ -22,7 +22,7 @@ build_msa = build() #instantiate the build object for file path, jekyll files
 AL_MSAs = ['33660']
 
 #report lists for testing
-selector.reports_to_run = ['A 3-1', 'D 3-1']
+#selector.reports_to_run = ['A 3-1', 'D 3-1']
 #selector.reports_to_run = ['A 11-1']
 #selector.reports_to_run = ['D 4-1', 'D 4-2', 'D 4-3', 'D 4-4', 'D 4-6', 'D 4-7']
 #selector.reports_to_run = ['A 5-1', 'A 5-2', 'A 5-3', 'A 5-4', 'A 5-5', 'A 5-7']
@@ -34,7 +34,7 @@ selector.reports_to_run = ['A 3-1', 'D 3-1']
 #selector.reports_to_run = ['B']
 
 #complete report list
-#selector.reports_to_run = ['A 3-1', 'A 3-2', 'A 4-1', 'A 4-2', 'A 4-3', 'A 4-4', 'A 4-5', 'A 4-6', 'A 4-7', 'A 5-1', 'A 5-2', 'A 5-3', 'A 5-4', 'A 5-5', 'A 5-7', 'A 7-1', 'A 7-2', 'A 7-3', 'A 7-4', 'A 7-5', 'A 7-6', 'A 7-7', 'A 8-1', 'A 8-2', 'A 8-3', 'A 8-4', 'A 8-5', 'A 8-6', 'A 8-7', 'A 9', 'A 11-1', 'A 11-2', 'A 11-3', 'A 11-4', 'A 11-5', 'A 11-6', 'A 11-7', 'A 11-8', 'A 11-9', 'A 11-10', 'A 12-1', 'A 12-2', 'A A-1', 'A A-2', 'A A-3', 'A A-4', 'A B'] #this needs to be changed to read from the input file
+selector.reports_to_run = ['A 3-1', 'A 3-2', 'A 4-1', 'A 4-2', 'A 4-3', 'A 4-4', 'A 4-5', 'A 4-6', 'A 4-7', 'A 5-1', 'A 5-2', 'A 5-3', 'A 5-4', 'A 5-5', 'A 5-7', 'A 7-1', 'A 7-2', 'A 7-3', 'A 7-4', 'A 7-5', 'A 7-6', 'A 7-7', 'A 8-1', 'A 8-2', 'A 8-3', 'A 8-4', 'A 8-5', 'A 8-6', 'A 8-7', 'A 9', 'A 11-1', 'A 11-2', 'A 11-3', 'A 11-4', 'A 11-5', 'A 11-6', 'A 11-7', 'A 11-8', 'A 11-9', 'A 11-10', 'A 12-1', 'A 12-2', 'A A-1', 'A A-2', 'A A-3', 'A A-4', 'A B'] #this needs to be changed to read from the input file
 #selector.reports_to_run = ['D 3-1', 'D 3-2', 'D 4-1', 'D 4-2', 'D 4-3', 'D 4-4', 'D 4-5', 'D 4-6', 'D 4-7', 'D 5-1', 'D 5-2', 'D 5-3', 'D 5-4', 'D 5-5', 'D 5-7', 'D 7-1', 'D 7-2', 'D 7-3', 'D 7-4', 'D 7-5', 'D 7-6', 'D 7-7', 'D 8-1', 'D 8-2', 'D 8-3', 'D 8-4', 'D 8-5', 'D 8-6', 'D 8-7', 'D 9', 'D 11-1', 'D 11-2', 'D 11-3', 'D 11-4', 'D 11-5', 'D 11-6', 'D 11-7', 'D 11-8', 'D 11-9', 'D 11-10', 'D 12-1', 'D 12-2', 'D A-1', 'D A-2', 'D A-3', 'D A-4', 'D B'] #this needs to be changed to read from the input file
 #selector.reports_to_run = ['D 11-1']
 for i in range(0, len(selector.reports_to_run)):
